366e7d1231b034aea013041afecd3221d38124c6
Kanishka Edirimanne <kanishka.edirimanne@datazentist.lk>
2024-10-31 17:34:53 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Vapi from "@vapi-ai/web";
 import './App.css';
 
+const vapi = new Vapi(process.env.REACT_APP_VAPI_PUBLIC_KEY);
+
 function App() {
+  const [isAnimating, setIsAnimating] = useState(false);
+  const [isSpeaking, setIsSpeaking] = useState(false); // Track if speaking
+
+  const handleButtonClick = () => {
+    if (isSpeaking) {
+      vapi.stop();
+    } else {
+      vapi.start(process.env.REACT_APP_VAPI_ASSISTANT_ID);
+    }
+
+    // Toggle the speaking state and play animation
+    setIsSpeaking(!isSpeaking);
+    setIsAnimating(true);
+    setTimeout(() => setIsAnimating(false), 500);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Testing LanguageBee</h2>
+      <img src="img.png" alt="company" />
+      <button
+        className={`speak-button ${isAnimating ? 'animate' : ''}`}
+        onClick={handleButtonClick}
+      >
+        {isSpeaking ? "Stop" : "Speak"}
+      </button>
     </div>
   );
 }
